2af6aa573baf55704935ca896e90785867120b6c
Oreo-Monster <ewrong16@gmail.com>
2021-06-19 10:56:31 -0600

    Finished GIF generator

--- ss.py
+++ ss.py
@@ -3,6 +3,11 @@ from spatialScan import SpatialScan
 import glob
 import re
 import numpy as np
+import PIL
+import io
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
 
 class SpatiallySeparated(SpatialScan):
 
@@ -38,8 +43,31 @@ class SpatiallySeparated(SpatialScan):
         sort = np.array(sort)
         idx = np.argsort(sort)
         dataObjs = dataObjs[idx]
+        IMobjs = []
+        for i, d in enumerate(dataObjs):
+            ax = d.visualize()
+            fig = ax.figure
+            str = f't = {sort[idx[i]]}ps'
+            ax.text(0.8, 0.9,str, horizontalalignment='center', verticalalignment='center', transform = ax.transAxes, fontsize=20)
+            im = SpatiallySeparated.fig2img(fig)
+            #im = PIL.Image.frombytes('RGB', fig.canvas.get_width_height(), fig.canvas.tostring_rgb())
+            IMobjs.append(im)
 
+        IMobjs[0].save(drPATH+'/spot.gif', format='GIF', append_images=IMobjs[1:], save_all=True, duration=500, loop=0)
+        
         return dataObjs
 
+    def fig2img(fig):
+        buf = io.BytesIO()
+        fig.savefig(buf)
+        buf.seek(0)
+        img = PIL.Image.open(buf)
+        return img
+
+    def vis3D(self):
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+
+        ax.plot(self.data[0], self.data[1], self.data[2])
 
 
